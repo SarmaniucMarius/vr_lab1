53501b44543af645350c4a58ab7ebcbf8c2c83e2
Eugen <euchiseliov@gmail.com>
2021-02-13 12:45:20 +0200

    Add select unselect feature Create new element based on combinations list

--- Assets/Scripts/Element.cs
+++ Assets/Scripts/Element.cs
@@ -5,33 +5,24 @@ using UnityEngine;
 public class Element : MonoBehaviour
 {
     public ElementType type;
+    public Side side;
     private Coroutine coroutine;
-
-    [HideInInspector]
-    public bool selected = false;
-    private bool focused = false;
 
     public void OnPointerEnter()
     {
         Debug.Log("Pointer enter: " + type.ToString());
-        focused = true;
-        if (!selected)
-        {
-            coroutine = StartCoroutine(SelectCoroutine());
-        }
+        coroutine = StartCoroutine(SelectCoroutine());
     }
 
     public void OnPointerExit()
     {
         Debug.Log("Pointer exit: " + type.ToString());
-        focused = false;
         StopCoroutine(coroutine);
     }
 
     private IEnumerator SelectCoroutine()
     {
-        yield return new WaitForSeconds(2f);
-        selected = true;
+        yield return new WaitForSeconds(1f);
         Game.current.SelectElement(this);
     }
 }
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -12,60 +12,179 @@ public enum ElementType
     WIND
 }
 
+public enum Side
+{
+    LEFT,
+    RIGHT
+}
+
+public class Combination
+{
+    public ElementType e1;
+    public ElementType e2;
+    public ElementType result;
+
+    public Combination(ElementType e1, ElementType e2, ElementType result)
+    {
+        this.e1 = e1;
+        this.e2 = e2;
+        this.result = result;
+    }
+
+    public bool match(ElementType ne1, ElementType ne2)
+    {
+        return e1 == ne1 && e2 == ne2 || e1 == ne2 && e2 == ne1;
+    }
+}
+
 public class Game : MonoBehaviour
 {
-    // {ELEM1, ELEM2, RESULT}
-    public ElementType[,] combinations = {
-        { ElementType.AIR, ElementType.AIR, ElementType.WIND}
-    };
-
-
-    ElementType[] elements = { ElementType.FIRE, ElementType.WATER, ElementType.EARTH, ElementType.AIR };
-    int step = 2;
-    // left shelf:  (-4, 1.5, -11)
-    // right shelf: ( 4, 1.5, -11)
-    public Transform initialPos;
-
     public GameObject newElementPrefab;
 
     public static Game current;
 
+    private Combination[] combinations = {
+        new Combination(ElementType.AIR, ElementType.AIR, ElementType.WIND)
+    };
+
+    private List<ElementType> elements = new List<ElementType>();
+    private int step = 2;
+
+    // left shelf:  (-4, 1.5, -11)
+    // right shelf: ( 4, 1.5, -11)
+
+    private Element leftElement = null;
+    private Element rightElement = null;
+
     private void Awake()
     {
         current = this;
-        for (int i = 0; i < elements.Length; i++)
-        {
-            GameObject left = Instantiate(newElementPrefab);
-            left.transform.Translate(new Vector3(-4f, 1.5f, -11f + i * step));
-            left.GetComponent<Element>().type = elements[i];
-
-            GameObject right = Instantiate(newElementPrefab);
-            right.transform.Translate(new Vector3(4f, 1.5f, -11f + i * step));
-            right.GetComponent<Element>().type = elements[i];
-            Debug.Log(elements[i].ToString());
-        }
+        createElement(ElementType.FIRE);
+        createElement(ElementType.WATER);
+        createElement(ElementType.EARTH);
+        createElement(ElementType.AIR);
     }
 
-    private GameObject newElement = null;
-    private Element previousSelectedElement = null;
+    private void createElement(ElementType type)
+    {
+        elements.Add(type);
+        CreateSidedElement(type, Side.LEFT, elements.Count - 1);
+        CreateSidedElement(type, Side.RIGHT, elements.Count - 1);
+    }
+
+    private GameObject CreateSidedElement(ElementType type, Side side, int offset)
+    {
+        GameObject obj = Instantiate(newElementPrefab);
+        obj.transform.Translate(new Vector3(GetXBySide(side), 1.5f, -11f + offset * step));
+        Element element = obj.GetComponent<Element>();
+        element.type = type;
+        element.side = side;
+        return obj;
+    }
+
+    private float GetXBySide(Side side)
+    {
+        if (side == Side.LEFT)
+        {
+            return 4f;
+        }
+        if (side == Side.RIGHT)
+        {
+            return -4f;
+        }
+        return 0;
+    }
+
     public void SelectElement(Element newSelectedElement)
     {
         Debug.Log("Select Element: " + newSelectedElement.type.ToString());
-        //if(previousSelectedElement != null && previousSelectedElement != newSelectedElement)
-        //{
-        //    // newElement = Instantiate(newElementPrefab, thirdElementPos);
+        if (newSelectedElement.side == Side.LEFT)
+        {
+            if (newSelectedElement == leftElement)
+            {
+                UnselectLeft();
+            }
+            else
+            {
+                UnselectLeft();
+                leftElement = newSelectedElement;
+                MarkAsSelected(leftElement);
+            }
+        }
 
-        //    newSelectedElement.isSelected = false;
-        //    previousSelectedElement.isSelected = false;
-        //    previousSelectedElement = null;
-        //}
-        //else
-        //{
-        //    if (newElement)
-        //        Destroy(newElement);
+        if (newSelectedElement.side == Side.RIGHT)
+        {
+            if (newSelectedElement == rightElement)
+            {
+                UnselectRight();
+            }
+            else
+            {
+                UnselectRight();
+                rightElement = newSelectedElement;
+                MarkAsSelected(rightElement);
+            }
+        }
 
-        //    previousSelectedElement = newSelectedElement;
-        //    newSelectedElement.isSelected = true;
-        //}
+
+        if (leftElement != null && rightElement != null)
+        {
+            tryCombination();
+        }
+    }
+
+    private void UnselectLeft()
+    {
+        if (leftElement != null)
+        {
+            MarkAsUnselected(leftElement);
+            leftElement = null;
+        }
+    }
+
+    private void UnselectRight()
+    {
+        if (rightElement != null)
+        {
+            MarkAsUnselected(rightElement);
+            rightElement = null;
+        }
+    }
+
+    private void MarkAsSelected(Element element)
+    {
+        element.gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+    }
+
+    private void MarkAsUnselected(Element element)
+    {
+        element.gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.black);
+    }
+
+    private void tryCombination()
+    {
+        Debug.Log("Try Combination: (" + leftElement.type.ToString() + ", " + rightElement.type.ToString() + ")");
+        Combination comb = Array.Find<Combination>(combinations, combination =>
+        {
+            return combination.match(leftElement.type, rightElement.type);
+        });
+        if (comb != null)
+        {
+            if (!elements.Contains(comb.result))
+            {
+                createElement(comb.result);
+                Debug.Log("Combination created: " + comb.result.ToString());
+                UnselectLeft();
+                UnselectRight();
+            }
+            else
+            {
+                Debug.Log("Combination already exists");
+            }
+        }
+        else
+        {
+            Debug.Log("No combination available");
+        }
     }
 }
